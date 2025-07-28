95638e51f895f9296bd44e1d1476d81b8dbf2705
Hunter Medney <hmedney@gmail.com>
2025-07-28 05:58:58 -0400

    cleanup

--- claude-container.sh
+++ claude-container.sh
@@ -17,13 +17,3 @@ docker run --rm -it \
   --workdir "${PWD}" \
   --env "HOME=/home/user" \
   ${IMAGE_NAME}
-
-
-  # --workdir "${PROJECT_DIR}" \
-# docker stop $CONTAINER_NAME
-# docker rm $CONTAINER_NAME
-# docker pull $IMAGE_NAME
-# docker container create --name $CONTAINER_NAME --net host -v /etc/homeassistant:/config $IMAGE_NAME
-# docker start $CONTAINER_NAME
-# docker update --restart always $CONTAINER_NAME
-# echo $FS_USER
